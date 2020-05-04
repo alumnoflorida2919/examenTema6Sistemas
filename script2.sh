2e54e2f5748a8ed95a50a3081a6d1dd123ca6006
alumno <alumno@pc-alumno.(none)>
2020-05-04 19:19:48 +0200

    termino script2

--- script2.sh
+++ script2.sh
@@ -1,1 +1,22 @@
 read -p "dime usuario" usuario
+
+cd etc
+maxl=`cat passwd | wc -l`
+n=1
+cont=0
+
+while [ $n -le $maxl ]; do
+    nombre=`cat passwd | awk '{print $1}' | head -$n | tail -1`
+    if [ $usario -eq $nombre ]; then
+	echo "el usuario existe"
+    else
+	cont=$(($cont+1))
+    fi
+    n=$(($n+1))
+done
+
+if [ $maxl -eq $cont ]; then
+    echo "el usuario no existe"
+fi
+
+    
