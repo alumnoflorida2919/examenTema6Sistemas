81417b3a269fea6b80b794c4a7188d82d0cdd1db
alumno <alumno@pc-alumno.(none)>
2020-05-04 18:31:53 +0200

    estoy utilizando un while

--- script1.sh
+++ script1.sh
@@ -1,7 +1,21 @@
 read -p "dime el nombre" nombre
 read -p "dime valor PID" pid
 
-for i in $(cat procesos.txt); do
-    nom=`cat procesos.txt | awk '{print$1}' | head -$n | tail -1`
+n=1
+maxl=`cat procesos.txt | wc -l`
 
-    if [ $nom -eq $nombre ]
+while [ $n -le $maxl ]; do
+
+    nom=`cat procesos.txt | awk '{print$1}' | head -$n | tail -1`
+    pid2=`cat procesos.txt | awk '{prin$2}' | head -$n | tail -1`
+
+    if [ $nom -eq $nombre ] && [ $pid2 -eq $pid ]; 
+    then
+	echo "el pid corresponde a ese usuario"
+    
+    fi
+    n=$(($n+1))
+done
+
+       
+
